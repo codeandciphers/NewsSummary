5c8f85799989602dad4bc85ed57a28c7a607de3a
Sarthak Sharma <sharmasarthak999@gmail.com>
2016-08-03 23:47:24 +0530

    Added NDTV support and Headings

--- bot2.py
+++ bot2.py
@@ -4,7 +4,7 @@ url=raw_input("Enter a website to extract the URL's from:")
 r=urllib2.urlopen(url)
 data=BeautifulSoup(r.read(),"html5lib").encode('utf-8')
 soup=BeautifulSoup(data,"html5lib")
-news=soup.find_all('div',class_="Normal")
+news=soup.find_all('div',class_="ins_storybody")
 ft=[]
 a=str(soup.html.title.prettify()).split("\n")
 a=a[1:]
@@ -14,7 +14,9 @@ ft+=a
 for item in news:
     [s.extract() for s in item('div')]
     [s.extract() for s in item('br')]
-    a=str(item.prettify()).split('\n')
+    [s.extract() for s in item('b')]
+    [s.extract() for s in item('p')]
+    a=str(item.prettify().encode('utf-8')).split('\n')
     a=a[1:]
     a.pop()
     ft+=a
